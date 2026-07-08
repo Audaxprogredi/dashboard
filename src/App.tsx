7c8c422938a585baf640b23bc87d50003fab184c
José Iván Salazar <josesala@espol.edu.ec>
2026-07-08 18:21:54 +0000

    actualizacion utl clase

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import './App.css'
+import HeaderUI from './components/HeaderUI';
 
 import { Grid } from '@mui/material';
+import AlertUI from './components/AlertUI';
 
 function App() {
 
@@ -8,11 +10,15 @@ function App() {
       <Grid container spacing={5} sx={{ justifyContent: "left", alignItems: "center" }}>
 
          {/* Encabezado */}
-         <Grid size={{ xs: 12, md: 12 }}>Elemento: Encabezado</Grid>
+         <Grid size={{ xs: 12, md: 12 }}>Elemento: Encabezado<HeaderUI/></Grid>
 
          {/* Alertas */}
-         <Grid size={{ xs: 12, md: 3 }}>Elemento: Alertas</Grid>
+         <Grid size={{ xs: 12, md: 3 }}>Elemento: Alertas</Grid>  <Grid 
+             container sx={{ justifyContent: "right", alignItems: "center" }}>
 
+             <AlertUI description="No se preveen lluvias"/>
+
+         </Grid>
          {/* Selector */}
          <Grid size={{ xs: 12, md: 9 }}>Elemento: Selector</Grid>
 
